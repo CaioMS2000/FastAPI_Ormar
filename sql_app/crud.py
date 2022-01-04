a4bf47dbf2f197269fe1b349f28e41ebd46ba838
Caio <caiocpe@gmail.com>
2022-01-04 00:28:35 -0300

    04/02/2022 00:28

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -9,14 +9,14 @@ import services.user as S_user
 
 async def get_user(user_id: int):
     # return db.query(models.User).filter(models.User.id == user_id).first()
-    res: models.User | None = await models.User.objects.get_or_none(id == user_id)
+    res: models.User | None = await models.User.objects.get_or_none(id= user_id)
     print('usou get_user pelo id:\n{}\n'.format(res), flush=True)
     return res
 
 
 async def get_user_by_nick(nick: str):
     # var = db.query(models.User).filter(models.User.nickname == nick).first()
-    res: models.User | None = await models.User.objects.get_or_none(nickname == nick)
+    res: models.User | None = await models.User.objects.get_or_none(nickname= nick)
     print('usou get_user pelo nick:\n{}\n'.format(res), flush=True)
     return res
 
@@ -37,17 +37,17 @@ async def get_messages(skip: int = 0, limit: int = 100):
 
 # creators
 async def create_user(user: schemas.User):
-    db_user = models.User(nickname=user.nickname, password=user.password)
-    # res: models.User | None = await db_user.save()
-    res: models.User | None = await db_user.obsjects.create()
+    # res: models.User | None = await models.User.obsjects.create(nickname=user.nickname, password=user.password)
+    print('entrou na criação\n', flush=True)
+    res = await models.User.objects.create(nickname=user.nickname, password=user.password)
     print('criou um usuario:\n{}\n'.format(res), flush=True)
     return res
 
 
-async def create_user_message(message: schemas.Message, owner_id: id):
-    user = get_user(owner_id)
-    db_message = models.Message(content=message.content, owner=user)
-    # res: models.Message | None = await db_message.save()
-    res: models.Message | None = await db_message.obsjects.create(content=message.content)
+async def create_user_message(message: schemas.Message):
+    user = await get_user(message.owner_id)
+    print(f'usuario usado na criação da mensagem\n{user}\n#\n')
+    # res: models.Message | None = await models.Message.objects.create(content=message.content, owner=user)
+    res = await models.Message.objects.create(content=message.content, owner=user)
     print('criou uma mensagem:\n{}\n'.format(res), flush=True)
     return res
